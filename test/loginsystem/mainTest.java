9f4bfa680569717094a0d76bdcc1808f12ca9118
lulu <luyandanyundu8@gmail.com>
2026-09-18 02:29:20 +0200

    Added comments to test file
    Updated the mainTest class to include proper formatting and comments.

--- test/loginsystem/mainTest.java
+++ test/loginsystem/mainTest.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
  */
+// Unit tests for the login system
 package loginsystem;
 
 import org.junit.After;
